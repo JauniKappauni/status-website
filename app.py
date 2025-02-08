5c1c39240dc6a132ca0e8a135480d7b609d9736b
JauniKappauni <social.jauni@gmail.com>
2025-02-08 16:37:22 +0100

    Added retrieval of form data

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from ping3 import verbose_ping
 app = Flask(__name__)
 @app.route("/", methods=["GET", "POST"])
 def hello_world():
+    host = request.form.get("host")
     return render_template("index.html")
 
 if __name__ == "__main__":
