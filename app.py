aea55287fd14954c1c74199f675730d272f996e6
JauniKappauni <social.jauni@gmail.com>
2025-02-08 16:26:22 +0100

    Added GET and POST method to route at /

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 from ping3 import verbose_ping
 
 app = Flask(__name__)
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def hello_world():
     return render_template("index.html")
 
