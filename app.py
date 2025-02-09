6cabe1afb6ec7850fb8d8f4ce752f206fbddf540
JauniKappauni <social.jauni@gmail.com>
2025-02-09 22:37:43 +0100

    Added declaration of variables with empty strings to prevent "UnboundLocalError" Error Added statement to prevent auto-run of code without pressing submit Added ping function with host variable to status variable Added statement to determine online status outputted as string for html tag

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 from flask import Flask, render_template, request
-from ping3 import verbose_ping
+from ping3 import ping
 
 app = Flask(__name__)
 @app.route("/", methods=["GET", "POST"])
 def hello_world():
-    host = request.form.get("host")
-    return render_template("index.html")
+    status = ""
+    output = ""
+    if request.method == "POST":
+        host = request.form.get("host")
+        status = ping(host)
+        output = "online" if status else "offline"
+    return render_template("index.html", output=output)
 
 if __name__ == "__main__":
     app.run(debug=True)
