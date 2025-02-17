15e32be7aece169d60c975b03c6cc42425165e13
JauniKappauni <social.jauni@gmail.com>
2025-02-17 20:22:09 +0100

    Added Configuration to run Flask on 0.0.0.0 at port 20004

--- app.py
+++ app.py
@@ -13,4 +13,4 @@ def hello_world():
     return render_template("index.html", output=output)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=20004, debug=True)
